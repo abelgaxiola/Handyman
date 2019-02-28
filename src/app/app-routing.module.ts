151376a8c42d667f35fe2f5bd91d63f5c45e2eae
Abel Gaxiola <abel.gaxiola@pima.gov>
2019-02-28 07:45:31 -0700

    Add meta tag with business info. Add logo image. Optimize by removing image.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import { ExamplesComponent } from "./examples/examples.component";
-import { ContactComponent } from "./contact/contact.component";
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
+import { ExamplesComponent } from './examples/examples.component';
+import { ContactComponent } from './contact/contact.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: "Home", component: HomeComponent },
-  { path: "Contact", component: ContactComponent },
-  { path: "Examples", component: ExamplesComponent },
-  { path: "", redirectTo: "Home", pathMatch: "full" },
-  { path: "**", redirectTo: "Home", pathMatch: "full" }
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'examples', component: ExamplesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
